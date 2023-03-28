82f33ff6670e6a561f853e5a4bfd7ccf81189361
fernandojbcba <benavidez.fernando@hotmail.com>
2023-03-28 11:55:50 -0300

    casi casi casi 28

--- service/client-service.js
+++ service/client-service.js
@@ -1,15 +1,17 @@
 const formulario = document.querySelector('[data-form]')
 
-formulario.addEventListener('submit', (evento) => {
-    evento.preventDefault()
-    const reg_nombre = document.querySelector('[data-nombre]').value
-    const reg_email = document.querySelector('[data-email]').value
-    const reg_pass = document.querySelector('[data-password]').value
+if (formulario) {
+    formulario.addEventListener('submit', (evento) => {
+        evento.preventDefault()
+        const reg_nombre = document.querySelector('[data-nombre]').value
+        const reg_email = document.querySelector('[data-email]').value
+        const reg_pass = document.querySelector('[data-password]').value
 
-    CrearUsuario(reg_nombre, reg_email, reg_pass)
-        .then((response) => console.log(response))
-        .catch(console.log)
-})
+        CrearUsuario(reg_nombre, reg_email, reg_pass)
+            .then((response) => console.log(response))
+            .catch(console.log)
+    })
+}
 
 const listarClientes = () =>
     fetch(`https://alurageekserver.onrender.com/usuario`).then((respuesta) =>
